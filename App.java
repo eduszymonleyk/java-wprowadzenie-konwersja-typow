7a066e3ff7b3d1bf7f3821eab45a1ce83cab1541
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-04 15:08:39 +0100

    zadania z konwersji typów

---
+++ App.java
@@ -0,0 +1,19 @@
+class App {
+	public static void main(String args[]){
+		// zadanie 1
+		int employees = 10420;
+		byte firsYearEmployees = 10;
+		
+		employees = firsYearEmployees;
+		System.out.println("Emplyees: "+employees);
+		
+		
+		// zadanie 2
+		int zakopane;
+		short number = 27266;
+		
+		zakopane = number;
+		System.out.println("Zakopane: "+zakopane);
+		
+	}
+}
